61cc2551799a42a66493fa2aee687af9b03d99a3
bps <bps@bps.go.id>
2026-03-10 10:44:30 +0800

    fix asset for production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,6 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'testuu@example.com',
-        ]);
-
         $this->call([
             AdminUserSeeder::class,
             MagangUserSeeder::class,
